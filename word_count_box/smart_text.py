af791e6e742228cffe7021f5f87c48122eda7139
Patrick Luck <archer920@gmail.com>
2018-11-13 20:46:02 -0500

    Control circulates through tabs

--- word_count_box/smart_text.py
+++ word_count_box/smart_text.py
@@ -2,69 +2,51 @@ from tkinter import *
 
 
 class SmartText(Text):
-    def __init__(self, master=None, cnf={}, fg='green', bg='black',
+    def __init__(self, master=None, next_widget=None, previous_widget=None, cnf={}, fg='green', bg='black', undo=True,
                  insertbackground='white', padx=10, pady=10, wrap=WORD, **kw):
-        super().__init__(master, cnf, fg=fg, bg=bg,
+        super().__init__(master, cnf, fg=fg, bg=bg, undo=True,
                          insertbackground=insertbackground, wrap=wrap, padx=padx, pady=10, **kw)
+        self.next_widget = next_widget
+        self.previous_widget = previous_widget
         self._key_bindings()
 
-
-    def undo(self):
-        print('undo')
-        pass
-
-    def redo(self):
-        print('redo')
-        pass
-
-    def cut(self):
-        print('cut')
-        pass
-
-    def copy(self):
-        print('copy')
-        pass
-
-    def paste(self):
-        print('paste')
-        pass
-
     def select_all(self):
-        print('select all')
         self.tag_add(SEL, '1.0', END + '-1c')
         self.mark_set(INSERT, '1.0')
         self.see(INSERT)
 
     def focus_next(self):
-        print('Next Item')
+        if self.next_widget:
+            self.next_widget.focus_set()
 
     def focus_previous(self):
-        print('Previous Item')
+        if self.previous_widget:
+            self.previous_widget.focus_set()
 
     def _key_bindings(self):
         self.bind('<Command-a>', lambda e: self.select_all())
         self.bind('<Control-a>', lambda e: self.select_all())
-
-        self.bind('<Command-z>', lambda e: self.undo())
-        self.bind('<Control-z>', lambda e: self.undo())
-
-        self.bind('<Command-y>', lambda e: self.redo())
-        self.bind('<Control-y>', lambda e: self.redo())
-
-        self.bind('<Command-x>', lambda e: self.cut())
-        self.bind('<Control-x>', lambda e: self.cut())
-
-        self.bind('<Command-c>', lambda e: self.copy())
-        self.bind('<Control-c>', lambda e: self.copy())
-
-        self.bind('<Command-v>', lambda e: self.paste())
-        self.bind('<Control-v>', lambda e: self.paste())
 
         self.bind('<Control-Tab>', lambda e: self.focus_next())
         self.bind('<Shift-Tab>', lambda e: self.focus_previous())
 
 
 if __name__ == '__main__':
-    tk = Tk()
-    st = SmartText(tk).pack()
+    f = Frame(Tk())
+    st = SmartText(f)
+    st2 = SmartText(f)
+    st3 = SmartText(f)
+
+    st.next_widget = st2
+
+    st2.next_widget = st3
+    st2.previous_widget = st
+
+    st3.previous_widget = st2
+
+    st.grid(row=0, column=0)
+    st2.grid(row=0, column=1)
+    st3.grid(row=0, column=2)
+
+    f.pack()
     mainloop()
